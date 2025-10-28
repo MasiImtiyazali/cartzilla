aedc9dd075d6d06ebaaef9e147059db7ee619dd0
MasiImtiyazali <afterclasses72@gmail.com>
2025-10-28 00:48:19 -0700

    Add files via upload

---
+++ Cartzilla/assets/js/main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
